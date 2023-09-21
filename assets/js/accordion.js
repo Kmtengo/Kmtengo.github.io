b1730dd05eafc5f25f6e06c8f2afba5767da1e39
Kmtengo <j12-4195-2019@student.mksu.ac.ke>
2023-09-21 13:17:20 +0300

    added all the files for mksu hackfest

---
+++ assets/js/accordion.js
@@ -0,0 +1,27 @@
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Machakos University
+  const position = { lat: 1.5309, lng: 37.2628 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Machakos University
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Machakos University
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Machakos University",
+  });
+}
+
+initMap();
