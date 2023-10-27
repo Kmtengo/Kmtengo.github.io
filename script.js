464b717a4f1e321d3a1201d9e77451305feabc6e
GraceWere <gwere2000@gmail.com>
2023-10-27 10:47:45 +0300

    Improve UI and responsiveness

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ function showDay(day) {
     selectedDay.forEach(dayElement => {
         dayElement.style.display = '';
     });
+
+    // Remove the "active-day-button" class from all day buttons
+    const dayButtons = document.querySelectorAll('.day-button');
+    dayButtons.forEach(button => {
+        button.classList.remove('active-day-button');
+    });
+
+    // Add the "active-day-button" class to the clicked day button
+    document.getElementById(`day${day}`).classList.add('active-day-button');
 }
 
 // Add event listeners to day buttons
@@ -15,5 +24,7 @@ document.getElementById('day1').addEventListener('click', () => showDay('1'));
 document.getElementById('day2').addEventListener('click', () => showDay('2'));
 document.getElementById('day3').addEventListener('click', () => showDay('3'));
 
-// Show the schedule for Day 1 by default
+// Show the schedule for Day 1 by default and highlight the button
 showDay('1');
+document.getElementById('day1').classList.add('active-day-button');
+
